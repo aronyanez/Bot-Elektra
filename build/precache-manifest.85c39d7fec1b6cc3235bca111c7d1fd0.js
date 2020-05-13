523128bce9407137bdea29dcad2781d946b54228
Arón Yáñez <aron.yanez@iaware.pro>
2020-05-13 13:59:52 -0500

    add /build for implementation azure

---
+++ build/precache-manifest.85c39d7fec1b6cc3235bca111c7d1fd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1763ef91237835ecbeac867a5be91d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "043bdf1cb94cb91ac6dc",
+    "url": "/static/css/main.a84885c3.chunk.css"
+  },
+  {
+    "revision": "f2f02225bbd64cff3346",
+    "url": "/static/js/2.3bbf6578.chunk.js"
+  },
+  {
+    "revision": "5cb31dd63c52789a5db36709ae864cc1",
+    "url": "/static/js/2.3bbf6578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "043bdf1cb94cb91ac6dc",
+    "url": "/static/js/main.a3324aaf.chunk.js"
+  },
+  {
+    "revision": "e45cadc96b9c351015bc",
+    "url": "/static/js/runtime-main.e0bafca3.js"
+  }
+]);
